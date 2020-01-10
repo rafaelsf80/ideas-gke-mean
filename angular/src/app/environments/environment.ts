ffe783c403154c52a005d6b5e120540bd1ebac98
Rafael Sanchez <rafaelsf80@gmail.com>
2020-01-10 23:44:20 +0100

    Removing some data

--- angular/src/app/environments/environment.ts
+++ angular/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     firebase: {
-      apiKey: "AIzaSyCqKllkp1rIjCnFEfcQi9slmp6z85w5DKg",
+      apiKey: "",
       authDomain: "third-pulsar-248314.firebaseapp.com",
       databaseURL: "YOUR_DATABASE_URL",
       projectId: "third-pulsar-248314",
